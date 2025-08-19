eefc900814c90ddb8b892bcdf63c199a7ee02a47
damian Buń <damian.bun00@gmail.com>
2025-08-19 17:31:53 +0200

    solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,98 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const {
+    fillTank,
+  } = require('./fillTank.js');
 
-  it('should ', () => {
+  it('should not return anthing ', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
 
+    expect(fillTank(customer, 10, 5)).toBeUndefined();
   });
 
-  // write tests here
+  it('should fuels full tank if amount is unknown', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 10);
+    expect(customer.vehicle.fuelRemains).toBe(40);
+  });
+
+  it('should not tank if amount is lower than two', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 10, 1);
+    expect(customer.money).toBe(3000);
+  });
+
+  it('should round purchased fuel price to the nearest hundredth part', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 10.111, 11);
+    expect(customer.money).toBe(2888.78);
+  });
+
+  it('should round the poured amount by discarding number to the tenth part', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 10, 10.17);
+    expect(customer.money).toBe(2899);
+  });
+
+  it('should always fill in only what the client can pay', () => {
+    const customer = {
+      money: 100,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 10, 12);
+    expect(customer.money).toBe(0);
+  });
+
+  it('should tank only what will fit if the amount is greater than tank can accomodate', () => {
+    const customer = {
+      money: 1000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 35,
+      },
+    };
+
+    fillTank(customer, 10, 10);
+    expect(customer.money).toBe(950);
+  });
 });
